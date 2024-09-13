90ae83e81c1a078758e51dac3579468aae8d97b2
espeonten <sydenteo@gmail.com>
2024-09-13 14:26:17 -0700

    Update move.js
    update to space again

--- move.js
+++ move.js
@@ -1,9 +1,9 @@
 function move() {
 
-if(keyDown("right") && p.x == width / 3) {
+if(keyWentDown("space") && p.x == width / 3) {
     p.x = width / 3 + width / 3
 }
-else if(keyDown("left") && p.x == width / 3 + width / 3) {
+else if(keyWentUp("space") && p.x == width / 3 + width / 3) {
     p.x = width / 3
 }
 }
